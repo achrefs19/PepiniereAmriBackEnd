9f1f06a768faf248ae292e10d2bcedad7c9ad5ed
achref <achref.smida@esprit.tn>
2024-06-08 08:42:42 +0100

    Initial commit

---
+++ models/employerModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const emplyerSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  advance: Number,
+  lastName: {
+    type: "String",
+  },
+  job: {
+    type: "String",
+  },
+  salary : Number,
+  hiringDate: {
+    type: "Date",
+    default: Date.now,
+  },
+  absenceCount: {
+    type: Number,
+    default: 0,
+  },
+  coefficient: Number,
+  CIN : String,
+  phoneNumber: Number,
+  adresse: {
+    type: String,
+  },
+  workStart: {
+    type: Date,
+    default: new Date(),
+  },
+  workEnd: {
+    type: Date,
+    default: Date.now,
+  },
+  selectedFile: String,
+});
+
+const Employers = mongoose.model("employers", emplyerSchema);
+export default Employers;
